c13f4684e1f7ac6bf67a79a3904bf1e8a9901e7c
Philippe Bolduc <pbolduc2354@gmail.com>
2018-06-06 17:59:49 +0000

    loop working, conditional working, everything work

--- countLetter.js
+++ countLetter.js
@@ -1,14 +1,17 @@
 function countLetters(string) {
-  var lettre = string.split('');
-  // return lettre
+  var lettre = string.split(" ").join("").split("");
   var compilation = {};
 
-  for (var i = 0; i < lettre.length; i++) {
-    compilation[lettre[i]] = lettre[i];
 
+  for (var i = 0; i < lettre.length; i++) {
+    if (compilation[lettre[i]] == undefined) {
+       compilation[lettre[i]] = 1;
+    } else {
+      compilation[lettre[i]] = compilation[lettre[i]] + 1;
+    }
   }return compilation
 }
 
 
 
-console.log(countLetters("lighthouse"));
+console.log(countLetters("lighthouse in the house"));
